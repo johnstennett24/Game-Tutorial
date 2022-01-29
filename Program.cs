45828a6659827a4e378c4ad39b154b641bad2831
johnstennett24 <jstennett830@gmail.com>
2022-01-28 23:32:51 -0700

    Worked on the Person class, then made the Director class as well.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace personal
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.startGame();
+        }
+    }
+}
